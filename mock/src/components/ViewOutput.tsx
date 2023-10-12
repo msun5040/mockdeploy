104b5b379cb0f34769dd664d797af538dc984ae4
syu0916 <sean_yu@brown.edu>
2023-10-11 22:57:57 -0400

    feat: table view implemented

--- mock/src/components/ViewOutput.tsx
+++ mock/src/components/ViewOutput.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react"
+import Table from 'react-bootstrap/Table';
 
 interface REPLOutputProps{
     // TODO: Fill this with desired props... Maybe something to keep track of the submitted commands
@@ -9,12 +10,12 @@ interface REPLOutputProps{
 
 interface ViewOutputProp {
     type: string
-    data: JSON
+    data: dataResponse
     error_message: string
 }
 
 interface dataResponse {
-    header: string[]
+    headers: string[]
     body: string[][]
 }
 
@@ -22,14 +23,41 @@ interface dataResponse {
 export function ViewOutput(prop: REPLOutputProps, responseMap: JSON | null) {
    
     let concatenatedResponse :JSX.Element;
-    if (responseMap == null) {
+    let convertedResponseMap: ViewOutputProp = responseMap as unknown as ViewOutputProp;
+    
+    // errored response maps should output nothing but an informative message.
+    if (responseMap == null || convertedResponseMap.type == "error") {
         concatenatedResponse = <div>No dataset has been loaded at this time.</div>
-    } else {
-        let convertedResponseMap: ViewOutputProp = responseMap as unknown as ViewOutputProp;
+    } 
 
-        // beautify this; make it into an html table. 
-        concatenatedResponse = <div>{JSON.stringify(convertedResponseMap.data)}</div>
-         
+    // creating the table
+    else {
+        let columns : JSX.Element[] = []
+        console.log(convertedResponseMap)
+
+        for (let i = 0; i < convertedResponseMap.data.headers.length; i ++) {
+            columns.push(<th>{convertedResponseMap.data.headers[i]}</th>)
+        }
+        let headers : JSX.Element = <thead><tr>{columns}</tr></thead>
+
+        // popoulating the rest of the data
+        let rows :JSX.Element[] = []
+        for (let i = 0; i < convertedResponseMap.data.body.length;i ++) {
+            let row : JSX.Element[] = [];
+            for (let j = 0; j < convertedResponseMap.data.body[0].length; j ++) {
+                row.push(<td>{convertedResponseMap.data.body[i][j]}</td>)
+            }
+            rows.push(<tr>{row}</tr>)
+        }
+        let data : JSX.Element = <tbody>{rows}</tbody>
+        let table: JSX.Element = 
+            <Table striped bordered hover>
+                {headers}
+                {data}
+            </Table>
+        
+
+        concatenatedResponse =  <div>{table}</div>     
     }
 
     prop.setResponses([...prop.responses, concatenatedResponse, <hr></hr>])
